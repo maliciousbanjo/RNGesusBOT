9c20ab20f0db580e4ea97acec4684e32cd6ea06c
maliciousbanjo <joebill79@gmail.com>
2018-02-07 22:11:02 -0600

    Updates

--- app.js
+++ app.js
@@ -7,9 +7,9 @@ const token = process.env.RNGESUS;
 
 client.on('ready', () => {
     console.log("Logged in as " + client.user.tag + "!");
-    //const id = client.channels.firstKey();
+    client.user.setActivity("Walmart-tier God");
     const myChannel = client.channels.get("126786138596704256");
-    myChannel.send("I am become reborn. Hail.");
+    //const myUser = client.users.get("126846346794762240");
 });
 
 client.on('message', msg => {
