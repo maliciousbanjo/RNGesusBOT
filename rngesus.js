86c90c7b3c5ce10e66ca6bbcaaaad759076dccea
Joseph Billstrom <joebill79@gmail.com>
2018-10-30 23:02:03 -0500

    spook

--- rngesus.js
+++ rngesus.js
@@ -10,6 +10,7 @@ const tokenTest = process.env.TESTING;
 const token = process.env.RNGESUS;
 
 let finalPhase = "";
+let rollCount = 0;
 
 client.on('ready', () => {
     console.log("Logged in as " + client.user.tag + "!");
@@ -25,7 +26,8 @@ client.on('ready', () => {
 
 client.on('message', msg => {
     if (msg.content.toLowerCase().includes("!roll")) {
-        msg.reply(roll());
+        //msg.reply(roll());
+        msg.reply(spookRoll());
     }
 
     if (msg.content.toLowerCase().includes("kek")) {
@@ -59,25 +61,13 @@ client.on('message', msg => {
     if (msg.content.toLowerCase().includes("!help")) {
         msg.channel.send(help());
     }
-    
-    if (msg.content.toLowerCase().includes("!pilgrim")) {
-        if (randomizerOdds(10)) {
-            insultMe().then(result => {
-                msg.channel.send(result);
-            });
+
+    if (msg.content.toLowerCase().includes("!hail")) {
+        if (msg.author.id == "126783002549157888") {
+            msg.channel.send("This meme has been brought to you buy Muff and Joe. Happy SpookTober 2018!");
         } else {
-            msg.channel.send(pilgrim());
+            return; // Do nothing
         }
-    }
-
-    if(msg.content.toLowerCase().includes("!insult")) {
-        insultMe().then(result => {
-            msg.channel.send(result);
-        });
-    }
-
-    if(msg.content.toLowerCase().includes("!test")) {
-        msg.reply(test());
     }
 });
 
@@ -106,8 +96,33 @@ function updatePhase() {
     client.user.setActivity("Ń̘̩̻̩͓̮o̯͚͍̬͈̻ͯͅT͉̀Ḥ̟̙̗̩͈̈́ͥͨͧͯ̐̅͞i̦̥̣̞̋ͭͬ̀n̘ͪ̄̿̿͞G̶̱͈͆ͭ͂ͯ̃̈̈ ̼̏̊c̷͉͍̜͓̮̮̙A̳̮̺͒͆ͫ̀͐Ņ̥͍͚̹̠͊̉ͥͣ ͚̫s̐҉̠͖̻̩̣̮̗a͈̰͈͚̼ͮ̎̃̈́v̵̳̲͔ͪ̂̔E̝̟̩͕̤̩̅̋ ̣ͧ̍̃́Y̼̩oͭ҉̤̹U͖̪͔̜̣̙̻͂ͦ̆͘");
 }
 
-function test() {
-    console.log("All hail the golden spooky");
+function spookRoll() {
+    // 0 1 2 3 4 5 6
+    switch(rollCount) {
+        case 0:
+            rollCount++;
+            return diacrit("God");
+        case 1:
+            rollCount++;
+            return diacrit("is");
+
+        case 2:
+            rollCount++;
+            return diacrit("dead");
+
+        case 3:
+            rollCount++;
+            return diacrit("there");
+        case 4:
+            rollCount++;
+            return diacrit("is");
+        case 5:
+            rollCount++;
+            return diacrit("only");
+        case 6:
+            rollCount = 0;
+            return diacrit("M U F F");
+    }
 }
 
 /**  
@@ -123,8 +138,15 @@ function roll() {
             return spooky.spookySass.phase3[Math.floor(Math.random()*spooky.spookySass.phase3.length)];
         }
     } else { // final phase has been triggered
-        const statement =  spooky.spookySass.phase4[Math.floor(Math.random()*spooky.spookySass.phase4.length)];
-        const emote = spooky.muffMotes[Math.floor(Math.random()*spooky.muffMotes.length)]
+        const statement =  spooky.spookySass.phase5[Math.floor(Math.random()*spooky.spookySass.phase5.length)];
+        let muffMotes = new Array(client.emojis.find(emoji => emoji.name === "muff1"));
+        muffMotes.push(client.emojis.find(emoji => emoji.name === "muff2"));
+        muffMotes.push(client.emojis.find(emoji => emoji.name === "muff3"));
+        muffMotes.push(client.emojis.find(emoji => emoji.name === "muff4"));
+        muffMotes.push(client.emojis.find(emoji => emoji.name === "muffrage"));
+        muffMotes.push(client.emojis.find(emoji => emoji.name === "muffW"));
+        muffMotes.push(client.emojis.find(emoji => emoji.name === "muff"));
+        const emote = muffMotes[Math.floor(Math.random()*muffMotes.length)]
         return diacrit(statement) + " " + emote;
     }
 }
@@ -133,33 +155,31 @@ function roll() {
  * Explicit command, return the Muffin face multi-emote
  */
 function muff() {
-    const response = "<:muff1:260651711381766144><:muff2:260651722114990082>\n"
-        + "<:muff3:260651732382646273><:muff4:260651744927809536>";
-    return response;
+    const muff1 = client.emojis.find(emoji => emoji.name === "muff1");
+    const muff2 = client.emojis.find(emoji => emoji.name === "muff2");
+    const muff3 = client.emojis.find(emoji => emoji.name === "muff3");
+    const muff4 = client.emojis.find(emoji => emoji.name === "muff4");
+
+    return `${muff1}${muff2}\n${muff3}${muff4}`;
 }
 
 /**  
  * Explicit command, return the inverted Muffin face multi-emote
  */
 function ffum() {
-    const response = "<:muff2:260651722114990082><:muff1:260651711381766144>\n"
-        +"<:muff3:260651732382646273><:muff4:260651744927809536>";
-    return response;
-}
+    const muff1 = client.emojis.find(emoji => emoji.name === "muff1");
+    const muff2 = client.emojis.find(emoji => emoji.name === "muff2");
+    const muff3 = client.emojis.find(emoji => emoji.name === "muff3");
+    const muff4 = client.emojis.find(emoji => emoji.name === "muff4");
 
-/**  
- * Explicit command, return the winged pilgrim
- */
-function pilgrim() {
-    const response = "<:wingL:347944440272519169><:pilgrim:347944396127469571><:wingR:347944450116288513>";
-    return response;
+    return `${muff2}${muff1}\n${muff3}${muff4}`;
 }
 
 /**
  * Explicit command, return the Monster Hunter World weakness chart
  */
 function weakness() {
-    const response = "https://monsterhunterworld.wiki.fextralife.com/file/Monster-Hunter-World/mhw_monster_weakness_chart.png";
+    const response = "YES, YOU ARE WEAK";
     return response;
 }
 
@@ -167,88 +187,27 @@ function weakness() {
  * Explicit command, return a list of all other available commands
  */
 function help() {
-    var embed = {embed: {
-        color: 3447003,
-        author: {
-          name: client.user.username,
-          icon_url: client.user.avatarURL
-        },
-        title: "GitHub Link",
-        url: "https://github.com/maliciousbanjo/RNGesusBOT",
-        description: "I am a just god, because I am unbiased.",
-        fields: [{
-            name: "Commands",
-            value: "!roll - Roll a 20-sided die\n" +
-            "!muff - Muffin face multi-emote\n" +
-            "!ffum - Inverted Muffin face multi-emote\n" + 
-            "!pilgrim - The winged pilgrim himself\n" +
-            "!weakness - Display the MHW Weakness Chart"
-          },
-        ],
-      }
-    };
-    return embed;
+    return "NOTHING CAN HELP YOU NOW";
 }
 
 /**  
  * Implicit command, called whenever someone says "kek"
  */
 function kekCzech(message) {
-    var chance = Math.floor(Math.random() * 20); // Golden kek
-    var goldenKek = false;
-    if (chance == 0) {
-        goldenKek = true;
-        console.log(message.author.username + " has received the Golden Kek");
-        // message.reply("http://i.imgur.com/Qvpx2KK.png"); // Standard Kek
-        var dirtyDate = JSON.stringify(new Date());
-        var currentDate = dirtyDate.substr(1, dirtyDate.length - 1);
-        if (currentDate < spooky.spookyDates.phase1) {
-            message.reply("", {file: new Discord.Attachment("./img/spooky_kek_s1.png", "p1.png")});
-            //message.reply(spooky.spooky.phase1);
-        } else if (currentDate >= spooky.spookyDates.phase1 && currentDate < spooky.spookyDates.phase2) {
-            message.reply("", {file: new Discord.Attachment("./img/spooky_kek_s2.png", "p2.png")});
-            //message.reply(spooky.spooky.phase2);
-        } else if (currentDate >= spooky.spookyDates.phase2 && currentDate < spooky.spookyDates.phase3) {
-            message.reply("", {file: new Discord.Attachment("./img/spooky_kek_s3.png", "p3.png")});
-            //message.reply(spooky.spooky.phase3);
-        } else if (currentDate >= spooky.spookyDates.phase3 && currentDate < spooky.spookyDates.phase4) {
-            if (finalPhase != true) {
-                updatePhase();
-                message.reply("", {file: new Discord.Attachment("./img/spooky_kek_s4.png", "p4.png")});
-                message.channel.send("<:muffrage:486743510587015168>"
-                    + diacrit("I RISE") + "<:muffrage:486743510587015168>");
-            } else {
-                message.reply("", {file: new Discord.Attachment("./img/spooky_kek_s4.png", "p4.png")});
-                const statement =  spooky.spookySass.phase4[Math.floor(Math.random()*spooky.spookySass.phase4.length)];
-                const emote = spooky.muffMotes[Math.floor(Math.random()*spooky.muffMotes.length)]
-                message.channel.send(diacrit(statement) + " " + emote);
-                //message.reply(spooky.spooky.phase4);
-            }
-        }
+    if (message.author.id == "126783002549157888" || message.author.id == "126783158803759104") {
+        message.reply("", {file: new Discord.Attachment("./img/spooky_kek_s5.png", "p5.png")});
+        const statement =  spooky.spookySass.phase5[Math.floor(Math.random()*spooky.spookySass.phase5.length)];
+        let muffMotes = new Array(client.emojis.find(emoji => emoji.name === "muff1"));
+        muffMotes.push(client.emojis.find(emoji => emoji.name === "muff2"));
+        muffMotes.push(client.emojis.find(emoji => emoji.name === "muff3"));
+        muffMotes.push(client.emojis.find(emoji => emoji.name === "muff4"));
+        muffMotes.push(client.emojis.find(emoji => emoji.name === "muffrage"));
+        muffMotes.push(client.emojis.find(emoji => emoji.name === "muffW"));
+        muffMotes.push(client.emojis.find(emoji => emoji.name === "muff"));
+        const emote = muffMotes[Math.floor(Math.random()*muffMotes.length)]
+        message.channel.send(diacrit(statement) + " " + emote);
+        return;
+    } else {
+        return;
     }
-    if (!(goldenKek)) {
-        chance = Math.floor(Math.random() * 250); // Cosmic kek
-        if (chance == 0) {
-            console.log(message.author.username + " has received the Cosmic Kek");
-            message.reply("http://i.imgur.com/MJ4QnXr.jpg");
-        }
-    }
-}
-
-/**
- * Provide a random insult
- */
-function insultMe() {
-    // Query the Insult API
-    const options = {
-        method: "GET",
-        uri: "https://insult.mattbas.org/api/insult.json",
-        json: true
-    }
-
-    return request(options)
-    .then(body => {
-        console.log(body);
-        return body.insult;
-    });
 }
