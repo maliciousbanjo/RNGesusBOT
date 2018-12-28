4f9ef4bb4cad5ad50cb14a6e916cf119fa15544c
Joseph Billstrom <joebill79@gmail.com>
2018-12-28 16:53:39 -0600

    Simple MongoDB transation

--- events/message.js
+++ events/message.js
@@ -1,10 +1,42 @@
+const mongo = require('mongodb');
 module.exports = (client, message) => {
     // Ignore bots
     if (message.author.bot) return;
+
+    // Process the message, parse out relevant metrics
+    const MongoClient = mongo.MongoClient;
+    console.log("Connecting to MongoDB...");
+    MongoClient.connect(client.config.databaseURL, {useNewUrlParser: true}, (error, client) => {
+        if (error) throw error;
+        console.log("Connected to MongoDB")
+        const db = client.db("test");
+
+        // Update the user's information. Will create new Document if it doesn't already exist
+        db.collection("users").updateOne(
+            // Discord User ID
+            { discord_id: message.author.id },
+            {
+                // Set username and avatar photo
+                $set: {
+                    username: message.author.username,
+                },
+                // Increment message count by 1. If field doesn't exist will instantiate at 1.
+                $inc: {
+                    messages: 1
+                }
+            },
+            { upsert: true },
+            (error, result) => {
+                if (error) throw error;
+                console.log("User Document Updated");
+                client.close();
+            }
+        );
+    });
     
     // COMMAND WITH ARGS
     if (message.content.indexOf(client.config.prefix) === 0) {
-        console.log("Command with args");
+        console.log('Command with args');
 
         const args = message.content.slice(client.config.prefix.length).trim().split(/ +/g);
         const command = args.shift().toLowerCase();
@@ -32,8 +64,8 @@ module.exports = (client, message) => {
     }
 
     // KEK CHECK
-    if (message.content.toLowerCase().includes("kek")) {
-        const kek = client.commands.get("kek");
+    if (message.content.toLowerCase().includes('kek')) {
+        const kek = client.commands.get('kek');
         kek.run(client, message);
     }
 };
