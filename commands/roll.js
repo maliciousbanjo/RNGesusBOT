29f17a2cfad64d34d2f5e4df69b0b7a7aeff68ab
Joseph Billstrom <joebill79@gmail.com>
2019-02-09 13:19:36 -0600

    Necessary limitations

--- commands/roll.js
+++ commands/roll.js
@@ -1,4 +1,8 @@
 exports.run = (client, message) => {
+    if (message.author.id === "234395566229028869") {
+        return;
+        // Rob is not allowed to roll Reeeee
+    }
     const roll = Math.floor(Math.random() * 21) + 1;
     if (roll != 21) {
         message.reply(roll);
