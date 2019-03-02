bd1ba70b92cfcd9c17d76726cfe970ded7fcf929
Joseph Billstrom <joebill79@gmail.com>
2019-03-02 14:59:59 -0600

    2.5.1

--- commands/kek.js
+++ commands/kek.js
@@ -1,24 +1,24 @@
 exports.run = (client, message) => {
     let chance = Math.floor(Math.random() * client.config.goldenRate); // Golden kek
     let goldenKek = false;
-    let plateletKek = false;
-    if (chance == 0) {
+    //let plateletKek = false;
+    if (chance == 0) { // Golden kek acheived
         goldenKek = true;
-        console.log(`${message.author.username} has received the Golden Kek`);
-        message.reply("http://i.imgur.com/Qvpx2KK.png"); // Golden Kek
-        goldenKekUpdate();
-        return;
-    }
-    if (!goldenKek) {
         chance = Math.floor(Math.random() * client.config.plateRate); // Platelet kek
-        if (chance == 0) {
+        if (chance == 0) { // Platelet kek steals the golden kek
             console.log(`${message.author.username} has had their kek stolen by a platelet!`);
             message.reply("https://i.imgur.com/izO82MU.png");
             plateletKekUpdate();
             return;
+        } else { // Normal golden kek
+            console.log(`${message.author.username} has received the Golden Kek`);
+            message.reply("http://i.imgur.com/Qvpx2KK.png"); // Golden Kek
+            goldenKekUpdate();
+            return;
         }
     }
-    if (!goldenKek && !plateletKek) {
+
+    if (!goldenKek) {
         chance = Math.floor(Math.random() * client.config.cosmicRate); // Cosmic kek
         if (chance == 0) {
             console.log(`${message.author.username} has received the Cosmic Kek`);
@@ -28,48 +28,7 @@ exports.run = (client, message) => {
         }
     }
 
-    /**
-     * DEPRECATED
-     * Update the user's kekcount and the most recent kek in MySQL
-     * @param {String} kekType 
-     */
-    function updateKek(kekType) {
-        const userQuery = `
-            UPDATE USER
-                SET ${[kekType]} = ${[kekType]} + 1
-            WHERE (discord_id = "${message.author.id}")
-        `;
-        client.sqlCon.query(userQuery, (error, result) => {
-            if (error) throw error;
-            // console.log(`${result.affectedRows} USER record(s) updated`);
-        });
-
-        let serverQuery;
-        if (kekType === "golden_kek") {
-            serverQuery = `
-                UPDATE SERVER
-                SET
-                    last_golden_user = "${message.author.id}",
-                    golden_timestamp = ${message.createdTimestamp}
-            `;
-            client.sqlCon.query(serverQuery, (error, result) => {
-                if (error) throw error;
-                // console.log(`${result.affectedRows} SERVER record(s) updated`);
-            });
-        } else { // cosmic_kek
-            serverQuery = `
-                UPDATE SERVER
-                SET
-                    last_cosmic_user = "${message.author.id}",
-                    cosmic_timestamp = ${message.createdTimestamp}
-            `;
-            client.sqlCon.query(serverQuery, (error, result) => {
-                if (error) throw error;
-                // console.log(`${result.affectedRows} SERVER record(s) updated`);
-            });
-        }
-    }
-
+    
     /**
      * Update the user's golden kek count and the most recent server kek in MySQL
      */
@@ -144,7 +103,7 @@ exports.run = (client, message) => {
                 client.sqlCon.query(userQuery, (error, result, fields) => {
                     if (error) throw error;
                     message.reply("Your golden kek has been stolen by a platelet!"+
-                    `\nYour Golden Kek count is now ${golden_kek_count}`)
+                    `\nYour Golden Kek count is now ${golden_kek_count - 1}`)
                 });
             } else {
                 message.reply("Your golden kek has been stolen by a platelet!")
