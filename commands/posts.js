b72aa3fe64decdc9baa844ac253974ef90dd6e98
maliciousbanjo <joebill79@gmail.com>
2019-12-30 13:25:33 -0600

    posts command update

--- commands/posts.js
+++ commands/posts.js
@@ -1,3 +1,4 @@
+const Discord = require('discord.js');
 exports.run = (client, message) => {
     const query = `
     SELECT user.*, count(message.author_id) AS 'message_count' 
@@ -16,9 +17,21 @@ exports.run = (client, message) => {
             return;
         }
 
-        // Fetch all the user profiles
-        const topUsers = new Array();
-        console.log(JSON.stringify(result));
+        // Format ranking strings
+        let userString = ``;
+        for (i = 0; i < result.length; i++) {
+            // Fetch the user profile from Discord
+            const user = message.guild.members.find(user => user.id === result[i].discord_id);
+            userString += `${i+1}. ${user}: ${record[i].message_count.toLocaleString()}\n`;
+        }
 
+        // Build the RichEmbed
+        const richEmbed = new Discord.RichEmbed()
+            .setColor('BLUE')
+            .setTitle(`Top Posters in ${message.guild.name}`)
+            .setThumbnail(message.guild.iconURL)
+            .addField('Users', userString);
+        
+        message.channel.send(richEmbed);s
     });
 }
